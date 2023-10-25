413799dcf80c9285638b13719f7fe4672146aa02
Xuyan Ru <xuyan.ru@lilly.com>
2023-10-24 20:42:47 -0400

    directly know the thirdparty folder path

--- src/build_third_party.sh
+++ src/build_third_party.sh
@@ -13,7 +13,7 @@ THREADS=8
 # the repo, or src. We want third party to be at the same level
 # as src.
 script_dir=$(dirname "$(readlink -f "$0")")
-third_party="${script_dir}/../third_party}"
+third_party="${script_dir}/../third_party"
 echo "third_party in ${third_party}"
 # Convert to full path name
 third_party=$(readlink -f ${third_party})
